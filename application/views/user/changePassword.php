5967b7246f4022add1f0983258acc0d3463270a5
GERARD <gerardspostbox@hotmail.com>
2015-10-14 14:12:11 +0200

    changePassword

---
+++ application/views/user/changePassword.php
@@ -0,0 +1,43 @@
+
+<html>
+<head>
+<title>PhotoSpace</title>
+</head>
+
+<body>
+
+<h3>Change Password</h3>
+<h5>
+<a href="index.php">
+Return to index.</a></h5>
+
+
+
+<table class="changePassword" align="center">
+    <form action="x.php" method="POST">
+    
+
+<tr>
+<td valign="middle" align="left"><p>Current Password:</p></td>
+<td valign="middle" align="right"><input type="password" name="currentPassword" /></td>
+</tr>
+
+<tr>
+<td valign="middle" align="left"><p>New Password:</p></td>
+<td valign="middle" align="right"><input type="password" name="newPassword1" /></td>
+</tr>
+
+<tr>
+<td valign="middle" align="left"><p>Repeat New Password:</p></td>
+<td valign="middle" align="right"><input type="password" name="newPassword2" /></td>
+</tr>
+
+<tr>
+    <td></td>
+    <td valign="middle" align="center"><input type="submit" value="Change Password" /></td>
+</tr>
+</form>
+</table>
+
+</body>
+</html>
